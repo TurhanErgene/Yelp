817dcdf4d7f4ec8286419c9184d01a4fac96a216
Turhan <turhan-ergene@hotmail.com>
2022-02-14 23:59:57 +0100

    router

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-const port = 3001
-
-app.listen(port || 3001, ()=> {
-  console.log(`server is listening on port ${port}`);
+app.get("/", (req, res) => {
+  console.log("get all restaurants");
 })
+
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
