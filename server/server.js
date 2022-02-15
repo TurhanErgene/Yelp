c67e9040b2dcb0ffbc7859f35b1ae1a368b5a7f6
Turhan <turhan-ergene@hotmail.com>
2022-02-15 01:24:55 +0100

    get and post routers

--- server/server.js
+++ server/server.js
@@ -4,11 +4,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("get all restaurants");
+
+
+// get restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: ["McDondalds", "Subway", "Dominos", "Köpenhamns Falafel"],
+    },
+  });
+});
+
+//get a restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req);
 })
 
-
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req);
+})
 
 const port = process.env.PORT || 4000;
 
