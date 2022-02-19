c8085f9ec7c89109c612c95e31d5fb9587622981
Turhan <turhan-ergene@hotmail.com>
2022-02-19 23:59:08 +0100

    AddRestaurant.jsx

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import AddRestaurant from '../components/AddRestaurant'
+import Header from '../components/Header'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <AddRestaurant/>
+    </div>
   )
 }
 
