a17552fa80f2d5e84cf4a2567f4a5d5618bd056c
Turhan <turhan-ergene@hotmail.com>
2022-02-19 23:46:46 +0100

    client routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/restaurant/:id/update" element={<UpdatePage />} />
+          <Route exact path="/restaurant/:id" element={<RestaurantDetailPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
