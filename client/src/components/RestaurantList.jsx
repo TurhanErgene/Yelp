0736cdedd95cd8c8c50168ad4a847598ab2a73ca
Turhan <turhan-ergene@hotmail.com>
2022-02-24 01:00:21 +0100

    restaurantList onClick error

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -18,6 +18,21 @@ const RestaurantList = (props) => {
     fetchData();
   }, []);
 
+  const handleDelete = async ( id) => {
+    try {
+      const response = await RestaurantFinder.delete(`/${id}`);
+      console.log(response);
+      
+      setRestaurants(
+        restaurants.filter((restaurant) => {
+          return restaurant.id !== id;
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="mx-auto">
       <table className="table table-hover">
@@ -44,7 +59,12 @@ const RestaurantList = (props) => {
                     <button className="btn btn-warning">Update</button>
                   </td>
                   <td>
-                    <button className="btn btn-danger">Delete</button>
+                    <button
+                      onClick={() => handleDelete(restaurant.id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               );
